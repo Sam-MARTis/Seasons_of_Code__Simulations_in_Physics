5a600ed11b89ee0c7c000a67212761b934e798db
Sam-MARTis <samanthmartis@gmail.com>
2024-07-11 10:55:54 +0530

    Attempt 2. Allocating and deallocating memory

--- Learning/Final_Project/QuadTrees_Fortran/core.f95
+++ Learning/Final_Project/QuadTrees_Fortran/core.f95
@@ -1,83 +1,110 @@
 module custom_types
     implicit none
 
-    type::Points
-        real:: mass = 1
-        real:: x = 200
-        real:: y = 200
-        real::vx = 10
-        real::vy = 10
-        real::size = 2
+    type :: Points
+        real :: mass = 1
+        real :: x = 200
+        real :: y = 200
+        real :: vx = 10
+        real :: vy = 10
+        real :: size = 2
+    end type Points
 
-    end type
+    type :: QuadTree
+        ! Properties
+        real :: x, y, width, height
 
-    type::QuadTree
+        ! Pointers to subtrees
+        type(QuadTree), pointer :: NW => null()
+        type(QuadTree), pointer :: NE => null()
+        type(QuadTree), pointer :: SE => null()
+        type(QuadTree), pointer :: SW => null()
 
-    !Properties
-    real::x, y, width, height
-
-    !Pointers to subtrees
-    type(QuadTree), pointer:: NW => null()
-    type(QuadTree), pointer:: NE => null()
-    type(QuadTree), pointer:: SE => null()
-    type(QuadTree), pointer:: SW => null()
-
-    !Data
-    logical:: isDivided = .false.
-    integer::pointsCount = 0
-    type(Points), dimension(4):: pointsArray
-
+        ! Data
+        logical :: isDivided = .false.
+        integer :: pointsCount = 0
+        type(Points), dimension(4) :: pointsArray
     end type QuadTree
-    
+
 contains
 
-recursive subroutine addPoints(self, point)
-    type(QuadTree), intent(inout) :: self
-    type(Points) , intent(inout):: point
-    if(((point%x>=self%x .and. point%x<(self%x + self%width)) .and. (point%y>=self%y .and. point%y<(self%y + self%width))) .eqv. .false.) then
-        return
-    end if
-    if (self%isDivided .eqv. .true.) then
-        call addPoints(self%NW, point)
-        call addPoints(self%NE, point)
-        call addPoints(self%SE, point)
-        call addPoints(self%SW, point)
-    else
-        if (self%pointsCount < 4) then
-            self%pointsArray(self%pointsCount + 1) = point
-            self%pointsCount = self%pointsCount + 1
-        else
-            call subdivide(self)
+    recursive subroutine addPoints(self, point)
+        type(QuadTree), intent(inout) :: self
+        type(Points), intent(inout) :: point
+
+        if (((point%x >= self%x .and. point%x < (self%x + self%width)) .and. (point%y >= self%y .and. point%y < (self%y + self%height))) .eqv. .false.) then
+            return
+        end if
+
+        if (self%isDivided) then
             call addPoints(self%NW, point)
             call addPoints(self%NE, point)
             call addPoints(self%SE, point)
             call addPoints(self%SW, point)
-    
+        else
+            if (self%pointsCount < 4) then
+                self%pointsArray(self%pointsCount + 1) = point
+                self%pointsCount = self%pointsCount + 1
+            else
+                call subdivide(self)
+                call addPoints(self%NW, point)
+                call addPoints(self%NE, point)
+                call addPoints(self%SE, point)
+                call addPoints(self%SW, point)
+            end if
         end if
-    end if
+    end subroutine addPoints
 
+    subroutine subdivide(self)
+        type(QuadTree), intent(inout) :: self
 
+        allocate(self%NW)
+        allocate(self%NE)
+        allocate(self%SE)
+        allocate(self%SW)
 
+        self%NW%x = self%x
+        self%NW%y = self%y
+        self%NW%width = self%width / 2
+        self%NW%height = self%height / 2
 
-end subroutine addPoints
+        self%NE%x = self%x + self%width / 2
+        self%NE%y = self%y
+        self%NE%width = self%width / 2
+        self%NE%height = self%height / 2
 
-function createTree() result(tree)
-    type(QuadTree) :: tree
-end function createTree
+        self%SE%x = self%x + self%width / 2
+        self%SE%y = self%y + self%height / 2
+        self%SE%width = self%width / 2
+        self%SE%height = self%height / 2
 
-subroutine subdivide(self)
-    type(QuadTree), intent(inout) :: self
-    type(QuadTree), pointer :: nw, ne, se, sw
+        self%SW%x = self%x
+        self%SW%y = self%y + self%height / 2
+        self%SW%width = self%width / 2
+        self%SW%height = self%height / 2
 
+        self%isDivided = .true.
+    end subroutine subdivide
 
-    self%NW => nw
-    self%NE => ne
-    self%SE => se
-    self%SW => sw
-end subroutine subdivide
+    recursive subroutine deallocateQuadTree(self)
+        type(QuadTree), intent(inout) :: self
 
-    
+        if (associated(self%NW)) then
+            call deallocateQuadTree(self%NW)
+            deallocate(self%NW)
+        end if
+        if (associated(self%NE)) then
+            call deallocateQuadTree(self%NE)
+            deallocate(self%NE)
+        end if
+        if (associated(self%SE)) then
+            call deallocateQuadTree(self%SE)
+            deallocate(self%SE)
+        end if
+        if (associated(self%SW)) then
+            call deallocateQuadTree(self%SW)
+            deallocate(self%SW)
+        end if
+    end subroutine deallocateQuadTree
+
 end module custom_types
-
-program main
-end program main
