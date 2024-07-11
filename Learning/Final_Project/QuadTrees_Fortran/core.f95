492fd777c66f31829537b4bd049dcb155c0f3ee4
Sam-MARTis <samanthmartis@gmail.com>
2024-07-11 08:59:20 +0530

    Trying to build the logic for adding points

--- Learning/Final_Project/QuadTrees_Fortran/core.f95
+++ Learning/Final_Project/QuadTrees_Fortran/core.f95
@@ -26,20 +26,32 @@ module custom_types
     logical:: isDivided = .false.
     integer::pointsCount = 0
     type(Points), dimension(4):: pointsArr
-
-    contains
-    procedure:: subdivide
-
-
-
     end type
     
 contains
 
+subroutine addPoints(self, point)
+    type(QuadTree), intent(inout) :: self
+    type(Points) , intent(inout):: point
+    if(((point%x>=self%x .and. point%x<(self%x + self%width)) .and. (point%y>=self%y .and. point%y<(self%y + self%width))) .eqv. .false.) then
+        return
+    end if
+    if (self%isDivided .eqv. .true.) then
+    end if
+
+
+
+end subroutine addPoints
+
 subroutine subdivide(self)
     type(QuadTree), intent(inout) :: self
+    type(QuadTree), target :: nw, ne, se, sw
 
-    
+
+    self%NW => nw
+    self%NE => ne
+    self%SE => se
+    self%SW => sw
 end subroutine subdivide
 
     
