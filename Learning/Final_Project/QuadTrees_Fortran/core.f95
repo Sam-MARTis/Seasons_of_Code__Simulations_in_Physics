eff4de6d2cc9a9d234808621680c0b088e8fc54c
Sam-MARTis <samanthmartis@gmail.com>
2024-07-18 20:07:13 +0530

    Completed array implementation of quad trees

--- Learning/Final_Project/QuadTrees_Fortran/core.f95
+++ Learning/Final_Project/QuadTrees_Fortran/core.f95
@@ -17,10 +17,7 @@ module custom_types
         real :: x, y, width, height
 
         ! Pointers to subtrees
-        type(QuadTree), pointer :: NW => null()
-        type(QuadTree), pointer :: NE => null()
-        type(QuadTree), pointer :: SE => null()
-        type(QuadTree), pointer :: SW => null()
+        type(QuadTree), dimension(:), allocatable:: subTrees
 
         ! Data
         logical :: isDivided = .false.
@@ -36,10 +33,15 @@ contains
     recursive subroutine addPoints(self, point)
         type(QuadTree), intent(inout) :: self
         type(Body), intent(in) :: point
+        integer:: iterVal
 
         logical:: isPointXInBounds, isPointYInBounds
         isPointXInBounds = (point%x >= self%x .and. point%x < (self%x + self%width))
         isPointYInBounds = (point%y >= self%y .and. point%y < (self%y + self%height))
+        !First check if point to add is in valid boundry
+        !If yes, then check if tree is divided
+        !If divided, add to subtrees
+        !If not divided, add to self
 
         if ((isPointXInBounds .and. isPointYInBounds) .eqv. .false.) then
             return
@@ -52,20 +54,18 @@ contains
         self%massContained = self%massContained + point%mass
 
         if (self%isDivided) then
-            call addPoints(self%NW, point)
-            call addPoints(self%NE, point)
-            call addPoints(self%SE, point)
-            call addPoints(self%SW, point)
+            do iterVal = 1, 4
+                call addPoints(self%subTrees(iterVal), point)
+            end do
         else
             if (self%pointsCount < PointsPerNode) then
                 self%pointsArray(self%pointsCount + 1) = point
                 self%pointsCount = self%pointsCount + 1
             else
                 call subdivide(self)
-                call addPoints(self%NW, point)
-                call addPoints(self%NE, point)
-                call addPoints(self%SE, point)
-                call addPoints(self%SW, point)
+                do iterVal = 1, 4
+                    call addPoints(self%subTrees(iterVal), point)
+                end do
             end if
         end if
     end subroutine addPoints
@@ -133,10 +133,10 @@ contains
                 end if
             end do
         else
-            pointsArrayNW = queryTreeRegionForPoints(tree%NW, rx1, ry1, width, height)
-            pointsArrayNE = queryTreeRegionForPoints(tree%NE, rx1, ry1, width, height)
-            pointsArraySE = queryTreeRegionForPoints(tree%SE, rx1, ry1, width, height)
-            pointsArraySW = queryTreeRegionForPoints(tree%SW, rx1, ry1, width, height)
+            pointsArrayNW = queryTreeRegionForPoints(tree%subTrees(1), rx1, ry1, width, height)
+            pointsArrayNE = queryTreeRegionForPoints(tree%subTrees(2), rx1, ry1, width, height)
+            pointsArraySE = queryTreeRegionForPoints(tree%subTrees(3), rx1, ry1, width, height)
+            pointsArraySW = queryTreeRegionForPoints(tree%subTrees(4), rx1, ry1, width, height)
             n = size(pointsArrayNW) + size(pointsArrayNE) + size(pointsArraySE) + size(pointsArraySW)
             allocate(pointsArray(n))
             do i = 1, size(pointsArrayNW)
@@ -163,7 +163,7 @@ contains
 
     subroutine subdivide(self)
         type(QuadTree), intent(inout) :: self
-        integer :: i
+        integer :: i, iterVal
         if (self%isDivided) then
             error stop "QuadTree is already divided"
         end if
@@ -171,61 +171,58 @@ contains
             error stop "QuadTree node has less points than its maximum node capacity. Not supposed to subdivide"
         end if
 
-        allocate(self%NW)
-        allocate(self%NE)
-        allocate(self%SE)
-        allocate(self%SW)
+        allocate(self%subTrees(4))
 
-        self%NW%x = self%x
-        self%NW%y = self%y
-        self%NW%width = self%width / 2
-        self%NW%height = self%height / 2
+        self%subTrees(1)%x = self%x
+        self%subTrees(1)%y = self%y
+        self%subTrees(1)%width = self%width / 2
+        self%subTrees(1)%height = self%height / 2
 
-        self%NE%x = self%x + self%width / 2
-        self%NE%y = self%y
-        self%NE%width = self%width / 2
-        self%NE%height = self%height / 2
+        self%subTrees(2)%x = self%x + self%width / 2
+        self%subTrees(2)%y = self%y
+        self%subTrees(2)%width = self%width / 2
+        self%subTrees(2)%height = self%height / 2
 
-        self%SE%x = self%x + self%width / 2
-        self%SE%y = self%y + self%height / 2
-        self%SE%width = self%width / 2
-        self%SE%height = self%height / 2
+        self%subTrees(3)%x = self%x + self%width / 2
+        self%subTrees(3)%y = self%y + self%height / 2
+        self%subTrees(3)%width = self%width / 2
+        self%subTrees(3)%height = self%height / 2
 
-        self%SW%x = self%x
-        self%SW%y = self%y + self%height / 2
-        self%SW%width = self%width / 2
-        self%SW%height = self%height / 2
+        self%subTrees(4)%x = self%x
+        self%subTrees(4)%y = self%y + self%height / 2
+        self%subTrees(4)%width = self%width / 2
+        self%subTrees(4)%height = self%height / 2
 
         self%isDivided = .true.
 
         do i = 1, self%pointsCount
-            call addPoints(self%NW, self%pointsArray(i))
-            call addPoints(self%NE, self%pointsArray(i))
-            call addPoints(self%SE, self%pointsArray(i))
-            call addPoints(self%SW, self%pointsArray(i))
+            do iterVal = 1, 4
+                call addPoints(self%subTrees(iterVal), self%pointsArray(i))
+            end do
         end do
     end subroutine subdivide
 
-    recursive subroutine deallocateQuadTree(self)
-        type(QuadTree), intent(inout) :: self
+    !!Not needed cause arrays used. Automatically deallocated when out of scope
+    ! recursive subroutine deallocateQuadTree(self)
+    !     type(QuadTree), intent(inout) :: self
 
-        if (associated(self%NW)) then
-            call deallocateQuadTree(self%NW)
-            deallocate(self%NW)
-        end if
-        if (associated(self%NE)) then
-            call deallocateQuadTree(self%NE)
-            deallocate(self%NE)
-        end if
-        if (associated(self%SE)) then
-            call deallocateQuadTree(self%SE)
-            deallocate(self%SE)
-        end if
-        if (associated(self%SW)) then
-            call deallocateQuadTree(self%SW)
-            deallocate(self%SW)
-        end if
-    end subroutine deallocateQuadTree
+    !     if (associated(self%NW)) then
+    !         call deallocateQuadTree(self%NW)
+    !         deallocate(self%NW)
+    !     end if
+    !     if (associated(self%NE)) then
+    !         call deallocateQuadTree(self%NE)
+    !         deallocate(self%NE)
+    !     end if
+    !     if (associated(self%SE)) then
+    !         call deallocateQuadTree(self%SE)
+    !         deallocate(self%SE)
+    !     end if
+    !     if (associated(self%SW)) then
+    !         call deallocateQuadTree(self%SW)
+    !         deallocate(self%SW)
+    !     end if
+    ! end subroutine deallocateQuadTree
 
 end module custom_types
 
@@ -261,7 +258,7 @@ program main
     call addPoints(root, Body(4, 4, 4, 4, 4))
     call addPoints(root, Body(4, 20, 200, 4, 4))
 
-    bodies= queryTreeRegionForPoints(root, 19.0, 190.0, 21.0, 10.1)
+    bodies= queryTreeRegionForPoints(root, 0.0, 0.0, 21.0, 10.1)
 
     
 
@@ -281,5 +278,5 @@ program main
 
 
 
-    call deallocateQuadTree(root)
+    ! call deallocateQuadTree(root)
 end program main
